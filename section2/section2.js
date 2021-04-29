116fcf4b7c3ee558ddc7f1cff75103f41e28226c
Micheal Sihavong <msihavong@eero.com>
2021-04-28 22:47:32 -0700

    initial commit

---
+++ section2/section2.js
@@ -0,0 +1,10 @@
+/* 
+--------------- What defines a JAMStack? --------------- 
+
+- M: static markups; HTML, CSS, JS
+- A: APIs
+- J: JS
+
+- the project must live in Git to be JAMStack, allowing it to be immutable 
+*/
+
